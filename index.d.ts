f96df5d67a97b326189c91a68d4e32903443063c
aleclarson <alec.stanford.larson@gmail.com>
2018-11-18 10:34:22 -0500

    add typings

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+type Logger = ILogger & IColored
+
+interface ILogger {
+  (...args: any[]): void
+  warn(...args: any[]): void
+  error(...args: any[]): void
+  debug(id: string): Logger
+  prefix(prefix: string): this
+  stylize(...args: any[]): string
+  trace(): void
+  clear(): void
+}
+
+interface IColored {
+  red(str: string): string
+  blue(str: string): string
+  green(str: string): string
+  yellow(str: string): string
+  cyan(str: string): string
+  pink(str: string): string
+  lred(str: string): string
+  lblue(str: string): string
+  lgreen(str: string): string
+  lyellow(str: string): string
+  lcyan(str: string): string
+  lpink(str: string): string
+  white(str: string): string
+  silver(str: string): string
+  gray(str: string): string
+  coal(str: string): string
+  black(str: string): string
+}
+
+declare module 'lodge' {
+  const log: Logger & {
+    cleanStack: (stack: string) => string
+  }
+  export default log
+}
